dbe548d7205f8e4981adc12be5e0faf962c2bf0f
nikoloss <rowland.lancer@gmail.com>
2013-12-13 21:26:06 +0800

    1

--- v1/src/main/java/v1/biz/User.java
+++ v1/src/main/java/v1/biz/User.java
@@ -3,17 +3,24 @@ package v1.biz;
 public class User {
 	private String name;
 	private String sex;
+	private String grade;
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this.name = name+"ss";
 	}
 	public String getSex() {
 		return sex;
 	}
 	public void setSex(String sex) {
 		this.sex = sex;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
 	}
 
 }
