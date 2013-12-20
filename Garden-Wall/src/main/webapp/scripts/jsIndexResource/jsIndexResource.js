046496e70e1ca4a2003ccc4b06577f273ce58847
vteamdell0723 <vteamdell0723@zhangjiajiang>
2013-12-20 17:10:40 +0800

    页面调整

--- Garden-Wall/src/main/webapp/scripts/jsIndexResource/jsIndexResource.js
+++ Garden-Wall/src/main/webapp/scripts/jsIndexResource/jsIndexResource.js
@@ -6,16 +6,28 @@ $(document).ready(function(){
 	     
 	     //初始化相应的模板文件.
 	     function initModel(){
-	     	  debugger;
 	     	    //定位相应的高宽比例.
-	     	    var  appbarHeight = $(".appbar").css("height").replace("px","")*1+10;
+	     	    var  appbarHeight = $(".topappbar").css("height").replace("px","")*1+25;
 	     	    $("#metreBox").offset({top:appbarHeight,left:0});
 	     	    $(".live-tile").liveTile();
+	     	    //移除相应的Class属性
+	     	    $(".live-tile").removeClass("carousel");
+	     	    //初始化相应的翻转List
+	     	    $("#mjhyBox").liveTile();
+	            //初始化相应的名家慧眼
+	             initmjhyBox();
 	     };
 	     
 	     //初始化相应的模板文件
 	     function bindEvent(){
 	     	 
 	     };
+	     
+	     function  initmjhyBox(){
+	     	     var  data = new Array();
+	     	     data.push({})
+	     	
+	     	
+	     }
 	
 });
