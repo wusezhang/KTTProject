dd531c4fce9885e0d158385838704e548039cada
vteamdell0723 <vteamdell0723@zhangjiajiang>
2013-12-20 18:05:20 +0800

    资源代码提交

--- Garden-Wall/src/main/webapp/scripts/jsIndexResource/jsIndexResource.js
+++ Garden-Wall/src/main/webapp/scripts/jsIndexResource/jsIndexResource.js
@@ -12,10 +12,10 @@ $(document).ready(function(){
 	     	    $(".live-tile").liveTile();
 	     	    //移除相应的Class属性
 	     	    $(".live-tile").removeClass("carousel");
-	     	    //初始化相应的翻转List
-	     	    $("#mjhyBox").liveTile();
 	            //初始化相应的名家慧眼
-	             initmjhyBox();
+	            initmjhyBox();
+	           //初始化相应的翻转List
+	     	    $("#mjhyBox").liveTile();
 	     };
 	     
 	     //初始化相应的模板文件
@@ -24,10 +24,16 @@ $(document).ready(function(){
 	     };
 	     
 	     function  initmjhyBox(){
-	     	     var  data = new Array();
-	     	     data.push({})
-	     	
-	     	
+	     	     var  data =[
+	                                {bzname:'沙黾农',bzjs:'股评专家',srclocation:'shajingrong.jpg'},
+	     	                        {bzname:'谢百三',bzjs:'复旦大学教授',srclocation:'xiebaisan.jpg'},
+	     	                        {bzname:'叶荣添',bzjs:'资深股评专家',srclocation:'yerongtian.jpg'},
+	     	                        {bzname:'郎咸平',bzjs:'资深经济学家',srclocation:'langxianping.jpg'}];
+	     	    for(var i=0 ;i<data.length ;i++){
+	     	    	  $("#mjhyBox").append("<li> <div class='well-small'>"
+						  +"<a href='#'><img class='full' src='image/celeBlogImage/"+data[i].srclocation+" '/></a> "	
+						   +" <div class='bottomshowInfor'> "+data[i].bzname+"("+data[i].bzjs+")" +" </div></div></li>");
+	     	    } 
 	     }
 	
 });
