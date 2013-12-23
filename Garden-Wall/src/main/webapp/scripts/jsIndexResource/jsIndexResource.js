19cf930f4304e91af39bbfe91d147ae91201733e
vteamdell0723 <vteamdell0723@zhangjiajiang>
2013-12-23 15:10:35 +0800

    页面调整

--- Garden-Wall/src/main/webapp/scripts/jsIndexResource/jsIndexResource.js
+++ Garden-Wall/src/main/webapp/scripts/jsIndexResource/jsIndexResource.js
@@ -15,7 +15,13 @@ $(document).ready(function(){
 	            //初始化相应的名家慧眼
 	            initmjhyBox();
 	           //初始化相应的翻转List
-	     	    $("#mjhyBox").liveTile();
+	     	    $("#mjhyBox").liveTile({
+	     	    	 startNow:false,
+	     	    	 flipListOnHover:true,
+	     	    	 flipListOnHoverEvent:'mouseover'
+	     	    });
+	     	   //移除相应的Class干扰
+	     	   $(".inforBox").removeAttr("flip-list");
 	     };
 	     
 	     //初始化相应的模板文件
@@ -24,16 +30,26 @@ $(document).ready(function(){
 	     };
 	     
 	     function  initmjhyBox(){
-	     	     var  data =[
-	                                {bzname:'沙黾农',bzjs:'股评专家',srclocation:'shajingrong.jpg'},
-	     	                        {bzname:'谢百三',bzjs:'复旦大学教授',srclocation:'xiebaisan.jpg'},
-	     	                        {bzname:'叶荣添',bzjs:'资深股评专家',srclocation:'yerongtian.jpg'},
+	     	     var  data =[{bzname:'沙黾农',bzjs:'股评专家，原现代快报副总编',srclocation:'shajingrong.jpg'},
+	     	                        {bzname:'谢百三',bzjs:'复旦大学教授，金融与资本市场研究中心主任',srclocation:'xiebaisan.jpg'},
+	     	                        {bzname:'叶荣添',bzjs:'股市分析家别人放弃，我坚持—叶荣添',srclocation:'yerongtian.jpg'},
 	     	                        {bzname:'郎咸平',bzjs:'资深经济学家',srclocation:'langxianping.jpg'}];
 	     	    for(var i=0 ;i<data.length ;i++){
-	     	    	  $("#mjhyBox").append("<li> <div class='well-small'>"
-						  +"<a href='#'><img class='full' src='image/celeBlogImage/"+data[i].srclocation+" '/></a> "	
-						   +" <div class='bottomshowInfor'> "+data[i].bzname+"("+data[i].bzjs+")" +" </div></div></li>");
+	     	    	  $("#mjhyBox").append("<li> <div class='accent cobalt '>"
+						    +" <a href='#'><img class='full' src='image/celeBlogImage/"+data[i].srclocation+" '/></a> "	
+						    +" <div class='bottomshowInfor'> "+data[i].bzname+" </div></div> "
+						    +" <div class='inforBox'> "
+						    +" <span class='tile-title'><span class='icon-white icon-bookmark'></span>"+data[i].bzname+"(简介)"+"</span> "
+						    + data[i].bzjs +" </div> </li>");
 	     	    } 
 	     }
 	
 });
+
+//自定义相应的链接的处理方法
+function gnzckxBindEvent(){
+	  $("#gnzckxBoxDiv").liveTile('play');
+}
+
+
+
