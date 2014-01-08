b7602d4f938ba2e7ff89f852c22a2d3e473183a0
unknown <vteamdell0723@.(none)>
2014-01-08 10:33:28 +0800

    Error

--- Garden-Wall/src/main/java/com/ktproject/cjhy/dao/impl/CjhyResourceDaoImpl.java
+++ Garden-Wall/src/main/java/com/ktproject/cjhy/dao/impl/CjhyResourceDaoImpl.java
@@ -1,10 +1,11 @@
 package com.ktproject.cjhy.dao.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.ktproject.cjhy.dao.CjhyResourceDao;
 import com.ktproject.cjhy.model.CjhyResourceModel;
 
@@ -18,7 +19,9 @@ public class CjhyResourceDaoImpl implements CjhyResourceDao {
 	}
 
 	public List<CjhyResourceModel> queryCjhyAuthorById(final int bzfl) {
-		return sqlSession.selectList("com.ktproject.cjhy.dao.impl.CjhyResourceDaoImpl.queryCjhyAuthorById", bzfl);
+		Map<String, Integer>  mapParam = new HashMap<String, Integer>();
+		mapParam.put("bzfl", bzfl);
+		return sqlSession.selectList("com.ktproject.cjhy.dao.impl.CjhyResourceDaoImpl.queryCjhyAuthorById", mapParam);
 	}
 
 }
