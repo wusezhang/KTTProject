7491dd267c1f5dcf44c186fc3f6a8d87d57bb343
unknown <vteamdell0723@.(none)>
2014-03-24 15:11:23 +0800

    sourceEditor

---
+++ Garden-Wall/src/main/java/com/ktproject/forexpic/controller/ForexpicController.java
@@ -0,0 +1,20 @@
+package com.ktproject.forexpic.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 汇市看图
+ * @author vteamdell0723
+ */
+@Controller
+@RequestMapping("ForexpicController")
+public class ForexpicController {
+         
+	private  Log  log = LogFactory.getLog(ForexpicController.class);
+			
+	
+			
+}
