5d9c33261ae62402af4c61e57b8cc5258d653571
unknown <vteamdell0723@.(none)>
2014-03-25 17:42:49 +0800

    sourceEditor

--- Garden-Wall/src/main/java/com/ktproject/forexpic/dao/impl/ForexPicDAOImpl.java
+++ Garden-Wall/src/main/java/com/ktproject/forexpic/dao/impl/ForexPicDAOImpl.java
@@ -1,12 +1,11 @@
 package com.ktproject.forexpic.dao.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import com.ktproject.forexpic.dao.ForexPicDAO;
 import org.apache.commons.logging.Log;
@@ -22,13 +21,10 @@ public class ForexPicDAOImpl implements ForexPicDAO {
 	
 	public List<Map<String, String>> queryAllForexPic() {
 		log.info("查询外汇看图的所有信息。");
-		RedisConnection conn = template.execute(new RedisCallback() {
-			public Object doInRedis(RedisConnection conns)
-					throws DataAccessException {
-                   return  conns;
-			}
-		});
-		System.out.println(conn);
+		
+		
+		
+		
 		return null;
 	}
 	
