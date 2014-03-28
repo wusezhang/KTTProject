4ecee00431a14cb56767ee8a32db980a04034dc1
unknown <vteamdell0723@.(none)>
2014-03-28 11:28:23 +0800

    sourceEditor

--- Garden-Wall/WebContent/views/scriptslib/forexpic/forexpic.js
+++ Garden-Wall/WebContent/views/scriptslib/forexpic/forexpic.js
@@ -5,14 +5,36 @@ $(document).ready(function(){
 	 });
 	 
 	 function initModel(){
-	 	  $.commonService("../../forexpicController/queryAllForexPic",'POST',{},function(data){
-		    	 alert(data);
+	 	  $.commonService('../../forexpicController/queryAllForexPic','POST',{},function(data){
+		    	initDataSet(data);
 		   });
 	 }
 	 
 	 function  bindEvent(){
 	 	
 	 }
+	
+	 function  initDataSet(data){
+	 	   var divRowInfor ;
+	 	   $('#firstRow').empty();
+	 	   $('#secondRow').empty();
+	 	   for(var i =0 ; i<8 ; i++){
+	 	   	    if(0<=i&&i<=3){
+	 	   	    	 $('#firstRow').append('<div class="col-xs-6 col-md-3">'
+						+ '<a  class="thumbnail"> <img src="'+data[i].imageUrl+'" class="img-rounded"> '
+						+ '<div class="caption bottomshowInfor"> '
+						+ ' 2014/04/06    外汇视图 '
+						+ ' </div> </a> </div>');
+	 	   	    }else{
+	 	   	    	 $('#secondRow').append('<div class="col-xs-6 col-md-3">'
+						+ '<a  class="thumbnail"> <img src="'+data[i].imageUrl+'" class="img-rounded"> '
+						+ '<div class="caption bottomshowInfor"> '
+						+ ' 2014/04/06    外汇视图 '
+						+ ' </div> </a> </div>');
+	 	   	    }
+	 	   }
+	 }
+	
 	
 	 
 });
