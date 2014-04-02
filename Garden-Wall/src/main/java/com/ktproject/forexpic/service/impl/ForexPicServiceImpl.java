bbcdd2e9cfed00fb9a357db541e9c931ecbdf6a7
wusezhang <wuse_zjj@163.com>
2014-04-02 22:06:27 +0800

    sourceEditor

--- Garden-Wall/src/main/java/com/ktproject/forexpic/service/impl/ForexPicServiceImpl.java
+++ Garden-Wall/src/main/java/com/ktproject/forexpic/service/impl/ForexPicServiceImpl.java
@@ -1,7 +1,6 @@
 package com.ktproject.forexpic.service.impl;
 
 import java.util.List;
-import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
