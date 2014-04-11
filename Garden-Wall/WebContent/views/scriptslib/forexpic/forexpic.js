dde3a643a8bfc0c249e37bfca23ed5ac589162b4
unknown <vteamdell0723@.(none)>
2014-04-11 16:08:24 +0800

    sourceEditor

--- Garden-Wall/WebContent/views/scriptslib/forexpic/forexpic.js
+++ Garden-Wall/WebContent/views/scriptslib/forexpic/forexpic.js
@@ -36,14 +36,14 @@ function  initDataSet(data){
 	 	   	    	 $('#firstRow').append('<div class="col-xs-5 col-md-3">'
 						+ '<a  class="thumbnail" href="javascript:showImageListModel(\''+ data[i].imageUrl+'.cnforex'+ '\')" >'
 						+ '<img src="'+data[i].imageUrl+'" class="img-rounded"> '
-						+ '<div class="caption bottomshowInfor"> '
+						+ '<div class="caption bottomshowInfor"> <span class="glyphicon glyphicon-calendar"> </span> '
 						+ data[i].title
 						+ ' </div> </a> </div>');
 	 	   	    }else if(3<i&&i<8){
 	 	   	    	 $('#secondRow').append('<div class="col-xs-5 col-md-3">'
 						+ '<a  class="thumbnail" href="javascript:showImageListModel(\''+ data[i].imageUrl+'.cnforex'+ '\')">' 
 						+ '<img src="'+data[i].imageUrl+'" class="img-rounded"> '
-						+ '<div class="caption bottomshowInfor"> '
+						+ '<div class="caption bottomshowInfor"> <span class="glyphicon glyphicon-calendar"> </span> '
 						+ data[i].title
 						+ ' </div> </a> </div>');
 	 	   	    }
--- Garden-Wall/WebContent/views/scriptslib/forexpic/forexpicList.js
+++ Garden-Wall/WebContent/views/scriptslib/forexpic/forexpicList.js
@@ -1,29 +1,39 @@
 $(document).ready(function() {
 	$(function() {
-        initModel();
-        bindEvent();
+		initModel();
+		bindEvent();
 	});
 
 	function initModel() {
-		
+
 		//初始化相应的查询信息.
-		if($.getParam('id')){
-			$.commonService('../../forexpicController/queryImageListById','POST',{id:$.getParam('id')},function(data){
-				initImageList(data);
+		if ($.getParam('id')) {
+			$.commonService('../../forexpicController/queryImageListById', 'POST', {
+				id : $.getParam('id')
+			}, function(data) {
+				initImageModel(data);
 			});
-		}else{
-			window.location.href='forexpic.html';
+		} else {
+			window.location.href = 'forexpic.html';
 		}
 
-		//做相应的图片初始化操作.
-		function initImageList(data) {
-			
-		}
+		//初始化fancybox
+		$('.fancybox').fancybox();
 
 	}
-	
-	function bindEvent(){
-		 
+
+	function bindEvent() {
+
+	}
+
+	function initImageModel(data) {
+		$('#showImageModel').empty();
+		for (var i = 0; i <data.length; i++) {
+			$('#showImageModel').append('<div class="col-xs-5 col-md-3"><a class="fancybox thumbnail" '
+			+ ' href="'+data[i].imageUrl+'" '
+			+ ' title="测试样例"> '
+			+ ' <img src="'+data[i].imageUrl+'" alt="" /> </a></div>');
+		}
 	}
 
 });
