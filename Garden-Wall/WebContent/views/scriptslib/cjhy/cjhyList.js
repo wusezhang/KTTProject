00a6a4951fb6bb03dceaf9e4cad3b0b5a18e465f
wusezhang <wuse_zjj@163.com>
2014-04-11 21:15:00 +0800

    sourceEditor

---
+++ Garden-Wall/WebContent/views/scriptslib/cjhy/cjhyList.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$(function(){
+		initModel();
+		bindEvent();
+	});
+	
+	function initModel(){
+		 //轮播初始化
+		 $('#articleTitleCarousel').carousel();
+	}
+	
+	function bindEvent(){
+		
+	}
+	
+	//初始化相应的作家列表信息.
+	function  initResourceList(){
+		//拿到相应存储的cook的值.
+		
+	}
+	
+});
