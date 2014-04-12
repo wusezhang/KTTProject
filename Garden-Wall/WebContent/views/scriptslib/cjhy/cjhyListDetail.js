f8afbca2b55935e6442de3330873db71eea7f0fb
wusezhang <wuse_zjj@163.com>
2014-04-12 18:22:44 +0800

    sourceEditor

--- Garden-Wall/WebContent/views/scriptslib/cjhy/cjhyListDetail.js
+++ Garden-Wall/WebContent/views/scriptslib/cjhy/cjhyListDetail.js
@@ -18,12 +18,19 @@ $(document).ready(function(){
 
 function  initArgs(){
 	return {url:'../../cjhy/queryCjhyResourceDetailById',
-		      params:{bzfl:0,countPerPage:12,id:$.getParam('id')},
+		      params:{bzfl:0,countPerPage:10,id:$.getParam('id')},
 		      callBack:function(data){initAuthorArticleList(data);}		
 		    };
 }
 
 function  initAuthorArticleList(data){
-	alert(data);
+	$('#authShowModel').empty();
+	for(var i = 0 ; i<data.length;i++){
+		  $('#authShowModel').append('<tr><td><img src="'+data[i].imageUrl+'" class="img-rounded imageShowModal"/></td>'
+		      +'<td><a href="'+data[i].linkUrl+'" class="btn btn-link">'+data[i].title+'</a></td>'
+          	  +'<td> '+$.formatLong(data[i].pubDate)+'</td> '
+          	  +'<td><a href="#" class="btn btn-sm btn-success">赞一个</a></td> '
+          	  +'</tr>');
+	}
 }
 
