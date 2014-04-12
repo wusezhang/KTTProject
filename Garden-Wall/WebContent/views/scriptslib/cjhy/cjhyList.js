0de0ca3a09a8b30f4c6644552a7b96b44f77e7e1
wusezhang <wuse_zjj@163.com>
2014-04-12 17:05:26 +0800

    sourceEditor

--- Garden-Wall/WebContent/views/scriptslib/cjhy/cjhyList.js
+++ Garden-Wall/WebContent/views/scriptslib/cjhy/cjhyList.js
@@ -36,9 +36,9 @@ function  initAuthorModel(data){
           	  +'<td> '+data[i].bzintroduce +'</td> '
           	  +'<td> '+$.formatLong(data[i].createDate)+'</td> '
           	  +'<td> <a href="#" class="btn btn-sm btn-success">60%</a></td> '
-          	  +'<td><a href="#" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-ok-sign">'
+          	  +'<td><a href="cjhyListDetail.html?id='+data[i].id+'"  class="btn btn-sm btn-primary fancybox fancybox.iframe"><span class="glyphicon glyphicon-ok-sign">'
           	  +'</span>&nbsp;&nbsp;评论列表</a></td></tr>'
           	  );
 	}
-	
+	$('.fancybox').fancybox();
 }
