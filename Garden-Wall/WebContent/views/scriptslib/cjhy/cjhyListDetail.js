7e3afac7b5e961384ef32ed49707463055d0ae71
wusezhang <wuse_zjj@163.com>
2014-04-12 19:16:09 +0800

    sourceEditor

--- Garden-Wall/WebContent/views/scriptslib/cjhy/cjhyListDetail.js
+++ Garden-Wall/WebContent/views/scriptslib/cjhy/cjhyListDetail.js
@@ -27,7 +27,7 @@ function  initAuthorArticleList(data){
 	$('#authShowModel').empty();
 	for(var i = 0 ; i<data.length;i++){
 		  $('#authShowModel').append('<tr><td><img src="'+data[i].imageUrl+'" class="img-rounded imageShowModal"/></td>'
-		      +'<td><a href="'+data[i].linkUrl+'" class="btn btn-link">'+data[i].title+'</a></td>'
+		      +'<td><a href="'+data[i].linkUrl+'" class="btn btn-link" target="view_window">'+data[i].title+'</a></td>'
           	  +'<td> '+$.formatLong(data[i].pubDate)+'</td> '
           	  +'<td><a href="#" class="btn btn-sm btn-success">赞一个</a></td> '
           	  +'</tr>');
