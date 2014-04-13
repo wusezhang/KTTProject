803f8bd5f824388eae0d1b2e7f76e0a4f26e6db7
wusezhang <wuse_zjj@163.com>
2014-04-13 16:21:02 +0800

    sourceEditor

--- Garden-Wall/WebContent/views/scriptslib/cjhy/hshyList.js
+++ Garden-Wall/WebContent/views/scriptslib/cjhy/hshyList.js
@@ -36,7 +36,8 @@ function  initAuthorModel(data){
           	  +'<td> '+data[i].bzintroduce +'</td> '
           	  +'<td> '+$.formatLong(data[i].createDate)+'</td> '
           	  +'<td> <a href="#" class="btn btn-sm btn-success">60%</a></td> '
-          	  +'<td><a href="#" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-ok-sign"> </span>&nbsp;&nbsp;评论列表</a></td></tr>');
+          	  +'<td><a href="hshyListDetail.html?id='+data[i].id+'"  class="btn btn-sm btn-primary fancybox fancybox.iframe"><span class="glyphicon glyphicon-ok-sign">'
+          	  +'</span>&nbsp;&nbsp;评论列表</a></td></tr>');
 	}
-	
+	$('.fancybox').fancybox();
 }
---
+++ Garden-Wall/WebContent/views/scriptslib/cjhy/hshyListDetail.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$(function(){
+		initModel();
+		bindEvent();
+	});
+	
+	function initModel(){
+	    //$.showPage(0);
+	}
+	
+	function bindEvent(){
+		
+	}
+	
+	
+	
+});
+
+function  initArgs(){
+	return {url:'../../cjhy/queryCjhyResourceDetailById',
+		      params:{bzfl:0,countPerPage:10,id:$.getParam('id')},
+		      callBack:function(data){initAuthorArticleList(data);}		
+		    };
+}
+
+function  initAuthorArticleList(data){
+	$('#authShowModel').empty();
+	for(var i = 0 ; i<data.length;i++){
+		  $('#authShowModel').append('<tr><td><img src="../imagelib/celeBlogImage/'+data[i].imageUrl+'"  class="img-rounded imageShowModal"/></td>'
+		      +'<td><a href="'+data[i].linkUrl+'" class="btn btn-link" target="view_window">'+data[i].title+'</a></td>'
+          	  +'<td> '+$.formatLong(data[i].pubDate)+'</td> '
+          	  +'<td><a href="#" class="btn btn-sm btn-success">赞一个</a></td> '
+          	  +'</tr>');
+	}
+}
+
