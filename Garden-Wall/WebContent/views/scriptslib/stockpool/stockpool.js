b82f7aed6f54e9ea6f6bde5782f3954a2c9f001f
unknown <vteamdell0723@.(none)>
2014-05-03 16:03:34 +0800

    sourceEditor

---
+++ Garden-Wall/WebContent/views/scriptslib/stockpool/stockpool.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	 $(function(){
+	 	initModal();
+	 	bindEvent();
+	 });
+	 
+	 function  initModal(){
+	 	//初始化相应的WIN8模块.
+		$(".live-tile").liveTile();
+		
+		
+		
+	 }
+	
+	 function  bindEvent(){
+	 	 
+	 }
+	
+});
