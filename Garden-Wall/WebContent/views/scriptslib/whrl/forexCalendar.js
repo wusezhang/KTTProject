58057a1f301d93f30173e5870b113d5c514d3bbe
unknown <vteamdell0723@.(none)>
2014-05-16 15:32:54 +0800

    SOURCEEDITOR

--- Garden-Wall/WebContent/views/scriptslib/whrl/forexCalendar.js
+++ Garden-Wall/WebContent/views/scriptslib/whrl/forexCalendar.js
@@ -1,45 +1,61 @@
-$(document).ready(function(){
-	$(function(){
+$(document).ready(function() {
+	$(function() {
 		initModel();
 		bindEvent();
 	});
-	
-	function initModel(){
-		
+
+	function initModel() {
 		//初始化分页相应的控件.
 		$.showPage(0);
-		
+
 		//初始化fancybox
 		$('.fancybox').fancybox();
-		
-		//定时任务机制.
-		setInterval('$.showPage(0)',120000);
-		
+        
+        //定时任务机制. 
+        timerCountDownEvent(); 
 	}
-	
-	function bindEvent(){
-		
-		
+
+	function bindEvent() {
+
 	}
-	
+
 });
 
-
-function  initArgs(){
-	 return {url:'../../forexCalendarController/queryForexCalendarResource',
-		      params:{countPerPage:25},
-		      callBack:function(data){initCurrentModel(data);}		
-		    };
+function initArgs() {
+	return {
+		url : '../../forexCalendarController/queryForexCalendarResource',
+		params : {
+			countPerPage : 25
+		},
+		callBack : function(data) {
+			initCurrentModel(data);
+		}
+	};
 }
 
-function  initCurrentModel(data){
-	 $('#forexNewsTable').empty();
-	 for(var i =0 ; i<data.length ;i++)
-	 $('#forexNewsTable').append('<tr>'
-	 +'<td>'+data[i].titleTime+'<td>'
-	 +'<td>'+data[i].descriptContext+'<td>'
-	 //+'<td>'+data[i].descriptDetails+'<td>'
-	 +'</tr>');
+function initCurrentModel(data) {
+	$('#forexNewsTable').empty();
+	for (var i = 0; i < data.length; i++)
+		$('#forexNewsTable').append('<tr>' 
+		+ '<td>' + data[i].titleTime + '<td>' 
+		+ '<td>' + data[i].descriptContext + '<td>'
+		// + '<td>'+data[i].descriptDetails+'<td>'
+		+'</tr>');
 }
 
+//时间戳 定义的调用时间为2分钟.
+function timerCountDownEvent(time) {
+	if (typeof time == 'undefined') {
+		time = 120;
+	   }
+	time = time - 1;
+	var value = ((120-time)/120)*100 +'%';
+	$('#showBar').css('width',value);
+	if (time >= 0) {
+		setTimeout("timerCountDownEvent(" + time + ")", 1000);
+	}else{
+		$.showPage(0);
+		timerCountDownEvent();
+	}
+}
 
