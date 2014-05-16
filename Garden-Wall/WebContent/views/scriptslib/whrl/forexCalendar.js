0d897728c4838810e68535118eae6018f1f4690a
unknown <vteamdell0723@.(none)>
2014-05-16 14:18:13 +0800

    SOURCEEDITOR

---
+++ Garden-Wall/WebContent/views/scriptslib/whrl/forexCalendar.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$(function(){
+		initModel();
+		bindEvent();
+	});
+	
+	function initModel(){
+		
+		//初始化分页相应的控件.
+		$.showPage(0);
+		
+		//初始化fancybox
+		$('.fancybox').fancybox();
+		
+	}
+	
+	function bindEvent(){
+		
+		
+	}
+	
+});
+
+
+function  initArgs(){
+	 return {url:'../../forexCalendarController/queryForexCalendarResource',
+		      params:{},
+		      callBack:function(data){initCurrentModel(data);}		
+		    };
+}
+
+function  initCurrentModel(data){
+	 $('#forexNewsTable').empty();
+	 for(var i =0 ; i<data.length ;i++)
+	 $('#forexNewsTable').append('<tr>'
+	 +'<td>'+data[i].titleTime+'<td>'
+	 +'<td>'+data[i].descriptContext+'<td>'
+	 //+'<td>'+data[i].descriptDetails+'<td>'
+	 +'</tr>');
+}
+
+
