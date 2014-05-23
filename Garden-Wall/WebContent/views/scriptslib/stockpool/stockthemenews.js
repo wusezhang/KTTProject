30ac61ac0367db37ae68b952228a89905f7b8eea
unknown <vteamdell0723@.(none)>
2014-05-23 15:08:08 +0800

    SOURCEEDITOR

--- Garden-Wall/WebContent/views/scriptslib/stockpool/stockthemenews.js
+++ Garden-Wall/WebContent/views/scriptslib/stockpool/stockthemenews.js
@@ -8,31 +8,44 @@ $(document).ready(function() {
     
 	function initModal() {
 		initThemeNews();
-		
+        //初始化相应的分页框架   
+	    $.showPage(0);
 	}
 
-	function bindEvent(){
-		
-	} 
-	
 	//初始化相应的数据集.
 	function initThemeNews(){
 	   $.commonService('../../stockThemeController/queryCurrentThemeNews', 'POST',
             {start:0,limit:13}, function(data) {
 		       initThemeTableInfor(data);
 	        }); 
-	
 	}
-	
+	//
 	function initThemeTableInfor(data){
 	    $('#showStockThemeModalList').empty();
 	    $.each(data,function(i,obj){
 	       $('#showStockThemeModalList').append('<tr>'
-	          +'<td>'+obj.title+'</td>'
+	          +'<td><span class="glyphicon glyphicon-hand-right"> </span>   '+obj.title+'</td>'
 	          +'<td>'+obj.pubDate+'</td>'
 	          +'</tr>');
 	    });
 	}
 	
 });
+//初始化调用参数信息.
+function  initArgs(){
+	return {url:'../../stockThemeController/queryCompanyNews',
+		      params:{countPerPage:11},
+		      callBack:function(data){initCompanyThemeNews(data);}		
+		    };
+}
+//初始化相应的公司板块信息.
+function  initCompanyThemeNews(data){
+   $('#companyNewsModal').empty();
+   $.each(data,function(i,obj){
+       $('#companyNewsModal').append('<a href="'+obj.linkUrl+'" class="list-group-item" target="view_window">'
+                +'<h5 class="list-group-item-heading">'
+				+'<span class="glyphicon glyphicon-hand-right"> </span>  '+obj.title+'</h5>'
+			    +'</a>');
+   });
+}
 
