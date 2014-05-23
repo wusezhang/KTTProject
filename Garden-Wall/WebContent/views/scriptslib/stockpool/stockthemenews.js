a6bc1143391625f094423604c18b5e6af21190b2
unknown <vteamdell0723@.(none)>
2014-05-23 17:50:26 +0800

    SOURCEEDITOR

--- Garden-Wall/WebContent/views/scriptslib/stockpool/stockthemenews.js
+++ Garden-Wall/WebContent/views/scriptslib/stockpool/stockthemenews.js
@@ -13,10 +13,46 @@ $(document).ready(function() {
 	    initCompanyNews();
 	}
 
+    function bindEvent(){
+    	$('#companyBtnDown').bind('click',companyNewsDownEvent);
+    	$('#companyBtnUp').bind('click',companyNewsUpEvent);
+    	$('#themeBtnDown').bind('click',themeNewsDownEvent);
+    	$('#themeBtnUp').bind('click',themeNewsUpEvent);
+    } 
+
+    function  companyNewsDownEvent(){
+    	$('#companyNewsCount').val(Number($('#companyNewsCount').val())+13);
+    	initCompanyNews();
+    }
+    
+    function  companyNewsUpEvent(){
+    	currentData = 0;
+    	if((Number($('#companyNewsCount').val())-13)>0){
+    		currentData = Number($('#companyNewsCount').val())-13;
+    	}
+    	$('#companyNewsCount').val(currentData);
+    	initCompanyNews();
+    }
+    
+    function themeNewsDownEvent(){
+    	$('#themeNewsCount').val(Number($('#themeNewsCount').val())+13);
+    	initThemeNews();
+    }
+    
+    function  themeNewsUpEvent(){
+    	currentData = 0;
+    	if((Number($('#themeNewsCount').val())-13)>0){
+    		currentData = Number($('#themeNewsCount').val())-13;
+    	}
+    	$('#themeNewsCount').val(currentData);
+    	initThemeNews();
+    }
+    
 	//初始化相应的数据集.
 	function initThemeNews(){
+	   startNum = Number($('#themeNewsCount').val())+0;
 	   $.commonService('../../stockThemeController/queryCurrentThemeNews', 'POST',
-            {start:0,limit:13}, function(data) {
+            {start:startNum,limit:13}, function(data) {
 		       initThemeTableInfor(data);
 	        }); 
 	}
@@ -25,15 +61,17 @@ $(document).ready(function() {
 	    $('#showStockThemeModalList').empty();
 	    $.each(data,function(i,obj){
 	       $('#showStockThemeModalList').append('<tr>'
-	          +'<td><span class="glyphicon glyphicon-hand-right"> </span>    '+obj.title+'</td>'
+	          +'<td class="text-primary"><span class="glyphicon glyphicon-hand-right"> </span>    '
+	          +' <a href="'+obj.linkUrl+'" target="view_window">'+obj.title+'</a></td>'
 	          +'<td>'+obj.pubDate+'</td>'
 	          +'</tr>');
 	    });
 	}
 	//初始化相应的上市公司信息.
 	function  initCompanyNews(){
+		startNum = Number($('#companyNewsCount').val())+0;
 	    $.commonService('../../stockThemeController/queryCompanyNews', 'POST',
-            {start:0,limit:13}, function(data) {
+            {start:startNum,limit:13}, function(data) {
 		       initCompanyThemeNews(data);
 	        }); 
 	   
@@ -43,7 +81,8 @@ $(document).ready(function() {
       $('#companyNewsModalList').empty();
       $.each(data,function(i,obj){
           $('#companyNewsModalList').append('<tr>'
-	          +'<td><span class="glyphicon glyphicon-hand-right"> </span>    '+obj.title+'</td>'
+	          +'<td class="text-danger"><span class="glyphicon glyphicon-hand-right"> </span>   '
+	          +' <a href="'+obj.linkUrl+'" target="view_window">'+obj.title+'</a></td>'
 	          +'<td>'+obj.pubDate+'</td>'
 	          +'</tr>');
       });
