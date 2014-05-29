d943008eb011aa18ce9925d0c5ec6deffb550625
unknown <vteamdell0723@.(none)>
2014-05-29 17:51:23 +0800

    SOURCEEDITOR

--- Garden-Wall/WebContent/views/scriptslib/comment/forexComment.js
+++ Garden-Wall/WebContent/views/scriptslib/comment/forexComment.js
@@ -5,7 +5,28 @@ $(document).ready(function(){
 	});
 	
 	function  bindEvent(){
-		
+		$('#forexNewsBtnDown').bind('click',forexNewsDownEvent);
+		$('#forexNewsBtnUp').bind('click',forexNewsUpEvent);
+	}
+	
+	function  forexNewsDownEvent(){
+		currentCount = 0 ;
+    	if((Number($('#forexNewsCount').val())+8)>Number($('#forexNewsTotalCount').val())){
+    		currentCount = Number($('#forexNewsCount').val());
+    	}else{
+    		currentCount = Number($('#forexNewsCount').val())+8;
+    	}
+    	$('#forexNewsCount').val(currentCount);
+    	initForexNewsDataSource();
+	}
+	
+	function  forexNewsUpEvent(){
+		currentData = 0;
+    	if((Number($('#forexNewsCount').val())-8)>0){
+    		currentData = Number($('#forexNewsCount').val())-8;
+    	}
+    	$('#forexNewsCount').val(currentData);
+    	initForexNewsDataSource();
 	}
 	
 	function  initModal(){
@@ -14,8 +35,10 @@ $(document).ready(function(){
 	}
 	
 	function  initForexNewsDataSource(){
+		startNum = Number($('#forexNewsCount').val())+0;
 		$.commonService('../../forexCommentsController/queryTodayForexComments', 'POST',
-            {start:0,limit:8}, function(map) {
+            {start:startNum,limit:8}, function(map) {
+               $('#forexNewsTotalCount').val(map.count);	
 		       initForexNewsModal(map.data);
 	        }); 
 	}
@@ -25,7 +48,7 @@ $(document).ready(function(){
 	    $.each(data,function(i,obj){
 	       $('#forexNewsModal').append('<a href="'+obj.linkUrl+'" class="list-group-item" target="view_window">'
 	             +'<h5 class="list-group-item-heading">'
-	             + obj.title+'</h5>'
+	             + obj.title+'<div class="pull-right">'+obj.pubDate+'</div> </h5>'
 				 +'<h6 class="list-group-item-text text-warning">'
 				 + obj.descriptContext
 				 +'</h6></a>');
