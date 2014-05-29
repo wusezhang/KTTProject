4fc8c00f9e9387111c60f63cc9ea702fa64e979f
unknown <vteamdell0723@.(none)>
2014-05-29 17:14:01 +0800

    SOURCEEDITOR

--- Garden-Wall/WebContent/views/scriptslib/comment/forexComment.js
+++ Garden-Wall/WebContent/views/scriptslib/comment/forexComment.js
@@ -21,7 +21,15 @@ $(document).ready(function(){
 	}
 	
 	function  initForexNewsModal(data){
-		alert(data);
+		$('#forexNewsModal').empty();
+	    $.each(data,function(i,obj){
+	       $('#forexNewsModal').append('<a href="'+obj.linkUrl+'" class="list-group-item" target="view_window">'
+	             +'<h5 class="list-group-item-heading">'
+	             + obj.title+'</h5>'
+				 +'<h6 class="list-group-item-text text-warning">'
+				 + obj.descriptContext
+				 +'</h6></a>');
+	    });
 	}
 	
 });
