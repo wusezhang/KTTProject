83f3eb65333630d583ff88f53d0f909af202305f
unknown <vteamdell0723@.(none)>
2014-05-29 14:56:32 +0800

    SOURCEEDITOR

--- Garden-Wall/WebContent/views/scriptslib/comment/forexComment.js
+++ Garden-Wall/WebContent/views/scriptslib/comment/forexComment.js
@@ -9,8 +9,19 @@ $(document).ready(function(){
 	}
 	
 	function  initModal(){
-		
+		//初始化相应的外汇评论信息
+		initForexNewsDataSource();
 	}
 	
+	function  initForexNewsDataSource(){
+		$.commonService('../../forexCommentsController/queryTodayForexComments', 'POST',
+            {start:0,limit:8}, function(map) {
+		       initForexNewsModal(map.data);
+	        }); 
+	}
+	
+	function  initForexNewsModal(data){
+		alert(data);
+	}
 	
 });
