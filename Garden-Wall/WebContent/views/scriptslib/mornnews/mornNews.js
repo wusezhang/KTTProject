c4a79c251780a9a4d7298311fcce955859b5ee94
wusezhang <wuse_zjj@163.com>
2014-06-15 09:40:19 +0800

    SOURCE

--- Garden-Wall/WebContent/views/scriptslib/mornnews/mornNews.js
+++ Garden-Wall/WebContent/views/scriptslib/mornnews/mornNews.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
+	$(function(){
+		bindEvent();
+		initModal();
+	});
+	
+	function  bindEvent(){
+		
+	}
+	
+	function  initModal(){
+		//初始化相应的股市评论信息
+		initChinaNewsDataSource();
+		
+	}
 	
 	
-	
+	function initChinaNewsDataSource(){
+		startNum = Number($('#chinaNewsCount').val())+0;
+		$.commonService('../../morningNews/queryChinaMorningNews', 'POST',
+            {start:startNum,limit:8}, function(map) {
+            	$('#chinaNewsTotalCount').val(map.count);
+                alert(map.data);
+	        }); 
+		
+		
+	}
 	
 });
