be297fc17141724f0dca4b04c00dcaf760525b6a
wusezhang <wuse_zjj@163.com>
2014-06-15 10:46:27 +0800

    SOURCE

--- Garden-Wall/WebContent/views/scriptslib/mornnews/mornNews.js
+++ Garden-Wall/WebContent/views/scriptslib/mornnews/mornNews.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 	});
 	
 	function  bindEvent(){
-		
+		$('#chinaNewsBtnDown').bind('click',chinaNewsDownEvent);
+		$('#chinaNewsBtnUp').bind('click',chinaNewsUpEvent);
 	}
 	
 	function  initModal(){
@@ -14,16 +15,75 @@ $(document).ready(function(){
 		
 	}
 	
+	/**
+	 *向下按钮点击事件. 
+	 */
+	function  chinaNewsDownEvent(){
+		currentCount = 0 ;
+    	if((Number($('#chinaNewsCount').val())+8)>Number($('#chinaNewsTotalCount').val())){
+    		currentCount = Number($('#chinaNewsCount').val());
+    	}else{
+    		currentCount = Number($('#chinaNewsCount').val())+8;
+    	}
+    	$('#chinaNewsCount').val(currentCount);
+    	initChinaNewsDataSource();
+	}
+	/**
+	 *向上按钮点击事件. 
+	 */
+	function  chinaNewsUpEvent(){
+		currentData = 0;
+    	if((Number($('#chinaNewsCount').val())-8)>0){
+    		currentData = Number($('#chinaNewsCount').val())-8;
+    	}
+    	$('#chinaNewsCount').val(currentData);
+    	initChinaNewsDataSource();
+	}
 	
 	function initChinaNewsDataSource(){
 		startNum = Number($('#chinaNewsCount').val())+0;
 		$.commonService('../../morningNews/queryChinaMorningNews', 'POST',
             {start:startNum,limit:8}, function(map) {
             	$('#chinaNewsTotalCount').val(map.count);
-                alert(map.data);
+                initChinaNewsModal(map.data);
 	        }); 
-		
-		
 	}
+	
+	/**
+	 * 初始化国内新闻版块.
+	 */
+	function  initChinaNewsModal(data){
+		$('#chinaNewsModal').empty();
+		var insertTable = '';
+		$.each(data,function(i,obj){
+		  if(i%2==0){
+		  	insertTable = insertTable +'<tr><td><div class="media">'
+	        +'<a class="pull-left" href="'+obj.linkUrl+'">'
+            +'<img class="media-object maxImageSize img-thumbnail" src='+obj.imageUrl+' alt="'+obj.title+'"></a>'
+			+'<div class="media-body">'
+			+'<h5 class="media-heading"><a href="'+obj.linkUrl
+			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
+			+obj.title+'</a>'
+			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
+			+'</div>'
+		    +'</div></td>';
+		  }else{
+		  	insertTable =insertTable + '<td><div class="media">'
+	        +'<a class="pull-left" href="'+obj.linkUrl+'">'
+            +'<img class="media-object maxImageSize img-thumbnail" src='+obj.imageUrl+' alt="'+obj.title+'"></a>'
+			+'<div class="media-body">'
+			+'<h5 class="media-heading"><a href="'+obj.linkUrl
+			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
+			+obj.title+'</a>'
+			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
+			+'</div>'
+		    +'</div></td></tr>';
+		  }
+	    });
+	    $('#chinaNewsModal').append(insertTable);
+	}
+	
 	
 });
