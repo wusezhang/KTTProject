c91cdc0a6b04a5d64647ba064ae345ec319ae632
wusezhang <wuse_zjj@163.com>
2014-06-16 23:00:39 +0800

    SOURCE

--- Garden-Wall/WebContent/views/scriptslib/mornnews/mornNews.js
+++ Garden-Wall/WebContent/views/scriptslib/mornnews/mornNews.js
@@ -134,7 +134,7 @@ $(document).ready(function(){
 			+'<h5 class="media-heading"><a href="'+obj.linkUrl
 			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
 			+obj.title+'</a>'
-			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<span class="label label-info pull-right">'+obj.pubDate+'</span> </h5>'
 			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
 			+'</div>'
 		    +'</div></td>';
@@ -146,7 +146,7 @@ $(document).ready(function(){
 			+'<h5 class="media-heading"><a href="'+obj.linkUrl
 			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
 			+obj.title+'</a>'
-			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<span class="label label-info pull-right">'+obj.pubDate+'</span> </h5>'
 			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
 			+'</div>'
 		    +'</div></td></tr>';
