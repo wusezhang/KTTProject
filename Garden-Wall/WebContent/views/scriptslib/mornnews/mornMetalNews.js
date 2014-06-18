37720d603c61a2bed4f72d57680cc0796499ea4c
unknown <vteamdell0723@.(none)>
2014-06-18 14:54:50 +0800

    SOURCE

--- Garden-Wall/WebContent/views/scriptslib/mornnews/mornMetalNews.js
+++ Garden-Wall/WebContent/views/scriptslib/mornnews/mornMetalNews.js
@@ -7,21 +7,33 @@ $(document).ready(function(){
 	function  bindEvent(){
 	    $('#forexNewsBtnDown').bind('click',forexNewsDownEvent);
 	    $('#forexNewsBtnUp').bind('click',forexNewsUpEvent);
+	    $('#metalNewsBtnDown').bind('click',metalNewsDownEvent);
+	    $('#metalNewsBtnUp').bind('click',metalNewsUpEvent);
 	}
 	
 	function  initModal(){
 		 //初始化外汇新闻.
-		initForexDateSource();
-		
+		initForexDataSource();
+		//初始化贵金属新闻.
+		initMetalDataSource();
 	}
 	
 	
-	function  initForexDateSource(){
+	function  initForexDataSource(){
 		startNum = Number($('#forexNewsCount').val())+0;
 		$.commonService('../../metalMorningNews/queryForexMorningNews', 'POST',
             {start:startNum,limit:8}, function(map) {
             	$('#forexNewsTotalCount').val(map.count);
                 initForexNewsModal(map.data);
+	       }); 
+	}
+	
+	function  initMetalDataSource(){
+		startNum = Number($('#metalNewsCount').val())+0;
+		$.commonService('../../metalMorningNews/queryMetalMorningNews', 'POST',
+            {start:startNum,limit:8}, function(map) {
+            	$('#metalNewsTotalCount').val(map.count);
+                initMetalNewsModal(map.data);
 	       }); 
 	}
 	
@@ -33,7 +45,7 @@ $(document).ready(function(){
     		currentCount = Number($('#forexNewsCount').val())+8;
     	}
     	$('#forexNewsCount').val(currentCount);
-    	initForexDateSource();
+    	initForexDataSource();
 	}
 	
 	function  forexNewsUpEvent(){
@@ -42,7 +54,27 @@ $(document).ready(function(){
     		currentData = Number($('#forexNewsCount').val())-8;
     	}
     	$('#forexNewsCount').val(currentData);
-    	initForexDateSource();
+    	initForexDataSource();
+	}
+	
+	function metalNewsDownEvent(){
+		currentCount = 0 ;
+    	if((Number($('#metalNewsCount').val())+8)>Number($('#metalNewsTotalCount').val())){
+    		currentCount = Number($('#metalNewsCount').val());
+    	}else{
+    		currentCount = Number($('#metalNewsCount').val())+8;
+    	}
+    	$('#metalNewsCount').val(currentCount);
+    	initMetalDataSource();
+	}
+	
+	function metalNewsUpEvent(){
+		currentData = 0;
+    	if((Number($('#metalNewsCount').val())-8)>0){
+    		currentData = Number($('#metalNewsCount').val())-8;
+    	}
+    	$('#metalNewsCount').val(currentData);
+    	initMetalDataSource();
 	}
 	
 	function initForexNewsModal(data){
@@ -57,7 +89,7 @@ $(document).ready(function(){
 			+'<h5 class="media-heading"><a href="'+obj.linkUrl
 			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
 			+obj.title+'</a>'
-			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<span class="label label-info pull-right">'+obj.pubDate+'</span> </h5>'
 			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
 			+'</div>'
 		    +'</div></td>';
@@ -69,7 +101,7 @@ $(document).ready(function(){
 			+'<h5 class="media-heading"><a href="'+obj.linkUrl
 			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
 			+obj.title+'</a>'
-			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<span class="label label-info pull-right">'+obj.pubDate+'</span> </h5>'
 			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
 			+'</div>'
 		    +'</div></td></tr>';
@@ -78,4 +110,37 @@ $(document).ready(function(){
 	    $('#forexNewsModal').append(insertTable);
 	}
 	
+	
+	function initMetalNewsModal(data){
+		$('#metalNewsModal').empty();
+		var insertTable = '';
+		$.each(data,function(i,obj){
+		  if(i%2==0){
+		  	insertTable = insertTable +'<tr><td><div class="media">'
+	        +'<a class="pull-left" href="'+obj.linkUrl+'">'
+            +'<img class="media-object maxImageSize img-thumbnail" src='+obj.imageUrl+' alt="'+obj.title+'"></a>'
+			+'<div class="media-body">'
+			+'<h5 class="media-heading"><a href="'+obj.linkUrl
+			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
+			+obj.title+'</a>'
+			+'<span class="label label-warning pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
+			+'</div>'
+		    +'</div></td>';
+		  }else{
+		  	insertTable =insertTable + '<td><div class="media">'
+	        +'<a class="pull-left" href="'+obj.linkUrl+'">'
+            +'<img class="media-object maxImageSize img-thumbnail" src='+obj.imageUrl+' alt="'+obj.title+'"></a>'
+			+'<div class="media-body">'
+			+'<h5 class="media-heading"><a href="'+obj.linkUrl
+			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
+			+obj.title+'</a>'
+			+'<span class="label label-warning pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
+			+'</div>'
+		    +'</div></td></tr>';
+		  }
+	    });
+	    $('#metalNewsModal').append(insertTable);
+	}
 });
