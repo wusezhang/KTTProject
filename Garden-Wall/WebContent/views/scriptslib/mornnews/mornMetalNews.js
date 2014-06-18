edd074d2dfa09660e9ec8f9fc0368b9da6617e7d
unknown <vteamdell0723@.(none)>
2014-06-18 13:55:39 +0800

    SOURCE

--- Garden-Wall/WebContent/views/scriptslib/mornnews/mornMetalNews.js
+++ Garden-Wall/WebContent/views/scriptslib/mornnews/mornMetalNews.js
@@ -5,12 +5,77 @@ $(document).ready(function(){
 	});
 	
 	function  bindEvent(){
-	
+	    $('#forexNewsBtnDown').bind('click',forexNewsDownEvent);
+	    $('#forexNewsBtnUp').bind('click',forexNewsUpEvent);
 	}
 	
 	function  initModal(){
+		 //初始化外汇新闻.
+		initForexDateSource();
 		
 	}
 	
+	
+	function  initForexDateSource(){
+		startNum = Number($('#forexNewsCount').val())+0;
+		$.commonService('../../metalMorningNews/queryForexMorningNews', 'POST',
+            {start:startNum,limit:8}, function(map) {
+            	$('#forexNewsTotalCount').val(map.count);
+                initForexNewsModal(map.data);
+	       }); 
+	}
+	
+	function  forexNewsDownEvent(){
+		currentCount = 0 ;
+    	if((Number($('#forexNewsCount').val())+8)>Number($('#forexNewsTotalCount').val())){
+    		currentCount = Number($('#forexNewsCount').val());
+    	}else{
+    		currentCount = Number($('#forexNewsCount').val())+8;
+    	}
+    	$('#forexNewsCount').val(currentCount);
+    	initForexDateSource();
+	}
+	
+	function  forexNewsUpEvent(){
+		currentData = 0;
+    	if((Number($('#forexNewsCount').val())-8)>0){
+    		currentData = Number($('#forexNewsCount').val())-8;
+    	}
+    	$('#forexNewsCount').val(currentData);
+    	initForexDateSource();
+	}
+	
+	function initForexNewsModal(data){
+		$('#forexNewsModal').empty();
+		var insertTable = '';
+		$.each(data,function(i,obj){
+		  if(i%2==0){
+		  	insertTable = insertTable +'<tr><td><div class="media">'
+	        +'<a class="pull-left" href="'+obj.linkUrl+'">'
+            +'<img class="media-object maxImageSize img-thumbnail" src='+obj.imageUrl+' alt="'+obj.title+'"></a>'
+			+'<div class="media-body">'
+			+'<h5 class="media-heading"><a href="'+obj.linkUrl
+			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
+			+obj.title+'</a>'
+			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
+			+'</div>'
+		    +'</div></td>';
+		  }else{
+		  	insertTable =insertTable + '<td><div class="media">'
+	        +'<a class="pull-left" href="'+obj.linkUrl+'">'
+            +'<img class="media-object maxImageSize img-thumbnail" src='+obj.imageUrl+' alt="'+obj.title+'"></a>'
+			+'<div class="media-body">'
+			+'<h5 class="media-heading"><a href="'+obj.linkUrl
+			+'" class="text-info" target="view_window"><span class="glyphicon glyphicon-globe"></span>  '
+			+obj.title+'</a>'
+			+'<span class="label label-primary pull-right">'+obj.pubDate+'</span> </h5>'
+			+'<h6 class="text-warning">'+$.trim(obj.descriptContext)+'</h6>'
+			+'</div>'
+		    +'</div></td></tr>';
+		  }
+	    });
+	    $('#forexNewsModal').append(insertTable);
+	}
 	
 });
