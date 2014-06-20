714eb2b21f4c823895b70f2ddc68390d5f6fb979
unknown <vteamdell0723@.(none)>
2014-06-20 12:36:43 +0800

    SOURCE

--- Garden-Wall/WebContent/views/scriptslib/datacenter/stockDataCenter.js
+++ Garden-Wall/WebContent/views/scriptslib/datacenter/stockDataCenter.js
@@ -8,28 +8,20 @@ $(document).ready(function() {
 	}
 
 	function goldForexShowEvent() {
-		     var currentdate = [];
-		     var forexdata = [];
-		     var golddata = [];
-		     $.commonService('../../dataCenter/queryForexGoldDataCenter', 'POST',
-             {start:0,limit:8}, function(map) {
-                  currentdate = map.currentdate;
-                  forexdata = map.forexdata;
-                  golddata = map.golddata;
-	         }); 
+		     var map = $.commonAsyncService('../../dataCenter/queryForexGoldDataCenter', 'POST',{start:0,limit:25}); 
 			 $('#showModal').highcharts(
 				{chart:{ type: 'areaspline' },
 				 title:{ text:'国家黄金和外汇储备 '},
 				 legend: { layout:'vertical', align:'left',verticalAlign: 'top', x: 150, y: 100, floating:true, borderWidth:1, backgroundColor:'#FFFFFF' },
-				 xAxis: { categories:currentdate, 
+				 xAxis: { categories:map.currentdate, 
 				 plotBands: [{
 				 color: 'rgba(68, 170, 213, .2)' }] },
 				 yAxis: { title: { text:'数量单位(千)'} },
 				 tooltip: { shared: true, valueSuffix:''}, 
 				 credits: { enabled: false },
 				 plotOptions: { areaspline: { fillOpacity: 0.5 } },
-				 series: [{ name: '国家外汇储备(亿美元)', data:forexdata},
-				          { name: '黄金储备(万盎司)', data:golddata}] 
+				 series: [{ name: '国家外汇储备(亿美元)', data:map.forexdata},
+				          { name: '国家黄金储备(万盎司)', data:map.golddata}] 
 				});
 	}
 
