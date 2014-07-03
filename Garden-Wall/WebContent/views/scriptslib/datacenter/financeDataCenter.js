281d5cedaade3133fb875400080d8a0a78a507fb
unknown <Administrator@.(none)>
2014-07-03 11:36:00 +0800

    SOURCE

--- Garden-Wall/WebContent/views/scriptslib/datacenter/financeDataCenter.js
+++ Garden-Wall/WebContent/views/scriptslib/datacenter/financeDataCenter.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	
 	function  bindEvent(){
 		$("#goldForexBtn").bind("click", goldForexShowEvent);
+		$('#bulkcargotransBtn').bind('click',bulkCargoTransShowEvent);
 	}
 	
 	function  initModel(){
@@ -37,6 +38,25 @@ $(document).ready(function() {
 				 series: [{ name: '国家外汇储备(亿美元)', data:map.forexdata},
 				          { name: '国家黄金储备(万盎司)', data:map.golddata}] 
 				});
+	}
+	
+	function bulkCargoTransShowEvent(){
+		 commenSelect('bulkcargotransBtn');
+		 var map = $.commonAsyncService('../../dataCenter/queryBulkCargoTransDataCenter', 'POST',{start:0,limit:20}); 
+		 $('#showModal').empty();
+		 $('#showModal').highcharts(
+			{chart:{ type: 'areaspline' },
+			 title:{ text:'波罗的海干货综合指数 '},
+			 legend: { layout:'vertical', align:'left',verticalAlign: 'top', x: 150, y: 100, floating:true, borderWidth:1, backgroundColor:'#FFFFFF' },
+			 xAxis: { categories:map.currenttime, 
+			 plotBands: [{
+			 color: 'rgba(68, 170, 213, .2)' }] },
+			 yAxis: { title: { text:'数量单位(千)'} },
+			 tooltip: { shared: true, valueSuffix:''}, 
+			 credits: { enabled: false },
+			 plotOptions: { areaspline: { fillOpacity: 0.5 } },
+			 series: [{ name: '波罗的海干货综合指数', data:map.indexvalue}] 
+			});
 	}
 
 });
